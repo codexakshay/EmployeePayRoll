28442065b6fbd4d4b614136f385d09259515d7c6
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-20 16:03:16 +0530

    EmployeePayRoll : Extened table with Employee Info

---
+++ EmployeePayRollService.sql
@@ -0,0 +1,49 @@
+create schema payroll_service;
+use payroll_service;
+
+CREATE TABLE employee_payroll (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    salary DOUBLE NOT NULL,
+    start DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO employee_payroll(name,salary,start) VALUES ('KANE',50000,'2020-12-30'),('KOHLI',40000,'2020-12-31'),('WARNER',60000,'2020-12-29');
+
+SELECT * FROM employee_payroll;
+
+SELECT salary FROM employee_payroll WHERE name = 'kane';
+
+SELECT salary FROM employee_payroll;
+
+SELECT * FROM employee_payroll WHERE start BETWEEN DATE(NOW()) AND CAST('2021-01-01' AS DATE);
+
+ALTER TABLE employee_payroll ADD gender char(1) after name;
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE employee_payroll SET gender = 'M';
+
+describe employee_payroll;
+
+SELECT sum(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender;
+SELECT min(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender;
+SELECT max(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender;
+SELECT avg(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender;
+
+INSERT INTO employee_payroll(name,salary,start) VALUES ('MITHALI',400000,'2020-12-30');
+INSERT INTO employee_payroll(name,salary,start) VALUES ('SMRITI',500000,'2020-11-17');
+UPDATE employee_payroll SET gender = 'F' WHERE name = 'SMRITI';
+
+SELECT gender,count(name) FROM employee_payroll GROUP BY gender;
+SELECT avg(salary) FROM employee_payroll WHERE gender = 'F' GROUP BY gender;
+
+SELECT * FROM employee_payroll;
+
+ALTER TABLE employee_payroll ADD PhoneNumber BIGINT after name;
+ALTER TABLE employee_payroll ADD Address varchar(100) after PhoneNumber;
+ALTER TABLE employee_payroll ADD Department varchar(50) NOT NULL after Address;
+
+
+
